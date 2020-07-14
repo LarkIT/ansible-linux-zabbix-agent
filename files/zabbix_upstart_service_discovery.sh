a14732e3a9beffcd39e983661abb06fbe8218f91
Ethan Claassen <ethan.claassen@lark-it.com>
2020-07-13 22:37:48 -0600

    Modified upstart service discovery script for CentOS 7 DEVSUPPORT-1767

--- files/zabbix_upstart_service_discovery.sh
+++ files/zabbix_upstart_service_discovery.sh
@@ -2,6 +2,6 @@
 # /usr/local/bin/zabbix_upstart_service_discovery.sh
 # Discover enabled services on the default runlevel on machines running the upstart init system
 
-service_list=$(chkconfig | grep 3:on | awk '{print $1}')
+service_list=$(chkconfig 2>/dev/null | grep 3:on | awk '{print $1}')
 
-echo -n '{"data":[';for s in ${service_list}; do echo -n "{\"{#SERVICE}\": \"$s\"},";done | sed -e 's:\},$:\}:';echo -n ']}' | grep '{"data":'
+echo -n '{"data":[';for s in ${service_list}; do echo -n "{\"{#SERVICE}\": \"$s\"},";done | sed -e 's:\},$:\}:';echo -n ']}'
